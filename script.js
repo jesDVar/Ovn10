44c721c374a86e619e4ace49d62ba9cca2b6c2cc
Jesse Varis <jessevaris@hotmail.com>
2022-01-21 10:58:25 +0100

    Trying to fetch

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let btn1 = document.querySelector('#addItemBtn');
+let textOutput = document.querySelector('#starWarsOutput');
+
+
+
 ((todo) => {
     todo.addItem = function(e){
         e.preventDefault();
@@ -10,8 +15,19 @@
         li.innerText = input.value;
         li.classList.add('#starWarsOutput');
     }
-
     
 })(window.todo = window.todo || {});
 
 document.querySelector('#starWarsForm').addEventListener('submit', todo.addItem);
+
+
+function getApi(){
+    /*Skriv din kod här*/
+    fetch('https://www.swapi.tech/api/people/?name=' + input)
+    .then(res => res.json())
+    .then(data =>
+    {
+    /*Och här*/
+    })
+    .catch(err => console.log(err))
+    }
