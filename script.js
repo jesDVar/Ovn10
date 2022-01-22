80bb3ee6decb8ff6db48e952e75d73ecdc057082
Jesse Varis <jessevaris@hotmail.com>
2022-01-22 18:38:25 +0100

    Testing around

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
-let btn1 = document.querySelector('#addItemBtn');
-let textOutput = document.querySelector('#starWarsOutput');
+//------------TESTFIELD-------------
+
+// let btn1 = document.querySelector('#addItemBtn');
+// let textOutput = document.querySelector('#starWarsOutput');
 
 
+// // btn1.addEventListener('click', function(){
 
-((todo) => {
-    todo.addItem = function(e){
-        e.preventDefault();
-        const input = this['sWUserInput'];
-        // console.log('hej');
-        // console.log(input);
-    }
-    const createSWInput = (input) => {
-        let li = document.createElement('li');
-        li.innerText = input.value;
-        li.classList.add('#starWarsOutput');
-    }
+// //     fetch('https://www.swapi.tech/api/people', requestOptions)
+// //     .then(response => response.text())
+// //     .then(result => console.log(result))
+// //     .catch(error => console.log('error', error));
+
+// // })
+
+
+// // ((todo) => {
+// //     todo.addItem = function(e){
+// //         e.preventDefault();
+// //         const input = this['sWUserInput'];
+// //         // console.log('hej');
+// //         // console.log(input);
+// //     }
+// //     const createSWInput = (input) => {
+// //         let li = document.createElement('li');
+// //         li.innerText = input.value;
+// //         li.classList.add('#starWarsOutput');
+// //     }
     
-})(window.todo = window.todo || {});
+// // })(window.todo = window.todo || {});
 
-document.querySelector('#starWarsForm').addEventListener('submit', todo.addItem);
+// // document.querySelector('#starWarsForm').addEventListener('submit', todo.addItem);
 
 
-// function getApi(){
+// function getApi() {
 //     /*Skriv din kod här*/
 //     fetch('https://www.swapi.tech/api/people/?name=' + input)
 //     .then(res => res.json())
-//     .then(data =>
+//     .then(data => 
+//         {
+
+//         })
+//     .catch(err => console.log('error', err))
+// }
     
-   
-//     console.log(res)
-//     console.log(result)
-//     )
-//     .catch(err => console.log('error', err));
-    
 
 
-    // fetch("https://www.swapi.tech/api/people", requestOptions)
-    // .then(response => response.text())
-    // .then(result => console.log(result))
-    // .catch(error => console.log('error', error));
+//     // fetch("https://www.swapi.tech/api/people", requestOptions)
+//     // .then(response => response.text())
+//     // .then(result => console.log(result))
+//     // .catch(error => console.log('error', error));
 
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
+//     // var requestOptions = {
+//     //     method: 'GET',
+//     //     redirect: 'follow'
+//     //   };
       
-      fetch("https://www.swapi.tech/api/people", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+//------------------NEW START---------------------
+//
+// let btn1 = document.querySelector('#addItemBtn');
+// let textOutput = document.querySelector('#starWarsOutput');
